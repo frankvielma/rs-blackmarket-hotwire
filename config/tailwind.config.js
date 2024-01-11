bcbf4aa85d90632005f207872ccf1af00e16eb43
Frank Vielma <frankvielma@gmail.com>
2024-01-11 14:47:24 -0400

    update tailwind config

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -12,6 +12,23 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        transparent: 'transparent',
+          current: 'currentColor',
+          'dark-violet': '#00031A',
+          'dark-gray': '#636363',
+          gray: '#C4C4C4',
+          'light-gray': '#E0E0E0',
+          background: '#F4F7FA',
+          'full-black': '#000000',
+          focus: '#1D76EF',
+          'tag-restored': '#559F21',
+          hover: '#446CBC',
+          'active-outline': '#9EBBF3',
+          'status-error': '#D42F1A',
+          links: '#076CE0',
+          active: '#254A96',
+      },
     },
   },
   plugins: [
