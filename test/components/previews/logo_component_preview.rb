e6cefc91f0e0fe50dad183807711a3a1238378cf
Frank Vielma <frankvielma@gmail.com>
2024-01-17 17:22:40 -0400

    user default layout on lookbook refactoring components

--- test/components/previews/logo_component_preview.rb
+++ test/components/previews/logo_component_preview.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class LogoComponentPreview < ViewComponent::Preview
-  layout 'component_preview'
-
   def default
     render(LogoComponent.new(title: 'Example component default'))
   end
