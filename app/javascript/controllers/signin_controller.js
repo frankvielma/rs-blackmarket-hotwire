672494013e3fd31791d0fcdaa3b683fcfc507b94
Frank Vielma <frankvielma@gmail.com>
2024-01-16 21:20:35 -0400

    initial change on sign in controller

---
+++ app/javascript/controllers/signin_controller.js
@@ -0,0 +1,13 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="signin"
+export default class extends Controller {
+  connect() {
+    
+  }
+
+  submit() {
+    
+  }
+  
+}
