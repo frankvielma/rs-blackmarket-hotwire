d1e4a5b4327fc77edf25ea2d76ca6ced391cc09b
Frank Vielma <frankvielma@gmail.com>
2024-01-17 17:22:40 -0400

    user default layout on lookbook refactoring components

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -85,4 +85,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.lookbook.project_name = 'Lookbook hotwire'
+  config.lookbook.preview_layout = 'component_preview'
 end
