e53a932e9ecbc18e0a1834af0ff879b6fa88cb27
Frank Vielma <frankvielma@gmail.com>
2024-01-22 17:06:32 -0400

    add signout add navbar

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,14 +21,14 @@
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
 #  provider               :string           default("email"), not null
-#  uid                    :string           default(""), not null
+#  client                 :string           default(""), not null
 #  tokens                 :json
 #
 # Indexes
 #
+#  index_users_on_client_and_provider   (client,provider) UNIQUE
 #  index_users_on_email                 (email) UNIQUE
 #  index_users_on_reset_password_token  (reset_password_token) UNIQUE
-#  index_users_on_uid_and_provider      (uid,provider) UNIQUE
 #
 
 class User < ApplicationRecord
