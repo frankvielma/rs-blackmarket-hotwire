1dec8a91cd844aa50edc786b0abc16fb3cfd4d5d
Frank Vielma <frankvielma@gmail.com>
2024-01-23 12:30:39 -0400

    fix error in sign_in when a user does not exist in db

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,33 +1,38 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  # This function signs in the user by finding the user with the given email,
-  # updating the user's access token and client, and then rendering a JSON
-  # response indicating success.
   def sign_in
     user = User.find_by(email: params[:email])
     access_token = params[:access_token]
     client = params[:client]
 
-    user.update!(tokens: access_token, client:)
+    if user.nil?
+      user = create_user(params)
+    else
+      user.update!(tokens: access_token, client:)
+    end
 
     session[:user_id] = user.id
     render json: { success: true }
   end
 
-  # Creates a new user with the provided email, access token, and client parameters and saves it to the database.
-  # Returns a JSON response indicating success.
   def sign_up
-    user = User.new(email: params[:email])
-    user.tokens = params[:access_token]
-    user.client = params[:client]
-    user.save!
-
+    create_user(params)
     render json: { success: true }
   end
 
   def sign_out
     session.delete(:user_id)
     redirect_to login_path
+  end
+
+  private
+
+  def create_user(params)
+    user = User.new(email: params[:email])
+    user.tokens = params[:access_token]
+    user.client = params[:client]
+    user.save!
+    user
   end
 end
