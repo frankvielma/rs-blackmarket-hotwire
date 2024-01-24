bfbb0944e1d1337dd92b221867e80ab52fba65de
Frank Vielma <frankvielma@gmail.com>
2024-01-24 10:48:15 -0400

    addtl udpates via rubocop

--- app/decorators/dashboard_decorator.rb
+++ app/decorators/dashboard_decorator.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class DashboardDecorator < Draper::Decorator
   delegate_all
 
@@ -9,5 +11,4 @@ class DashboardDecorator < Draper::Decorator
   #       object.created_at.strftime("%a %m/%d/%y")
   #     end
   #   end
-
 end
--- spec/decorators/error_decorator_spec.rb
+++ spec/decorators/error_decorator_spec.rb
@@ -1,4 +1,3 @@
-require 'rails_helper'
+# frozen_string_literal: true
 
-RSpec.describe ErrorDecorator do
-end
+require 'rails_helper'
