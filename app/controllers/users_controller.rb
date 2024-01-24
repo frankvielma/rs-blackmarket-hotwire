4cb4249941177ae26c7641414299627e3ec6ee9c
Frank Vielma <frankvielma@gmail.com>
2024-01-24 11:23:31 -0400

    refactor users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,15 +3,7 @@
 class UsersController < ApplicationController
   def sign_in
     user = User.find_by(email: params[:email])
-    access_token = params[:access_token]
-    client = params[:client]
-
-    if user.nil?
-      user = create_user(params)
-    else
-      user.update!(tokens: access_token, client:)
-    end
-
+    user = update_create_user(user, params)
     session[:user_id] = user.id
     render json: { success: true }
   end
@@ -27,6 +19,15 @@ class UsersController < ApplicationController
   end
 
   private
+
+  def update_create_user(user, params)
+    if user.nil?
+      user = create_user(params)
+    else
+      user.update!(tokens: params[:access_token], client: params[:client])
+    end
+    user
+  end
 
   def create_user(params)
     user = User.new(email: params[:email])
