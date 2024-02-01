5a57dfc508c0c4c3d56c82ce430c63f824597011
Frank Vielma <frankvielma@gmail.com>
2024-02-01 11:47:54 -0400

    setting up motor admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  mount Motor::Admin => '/motor_admin'
   devise_for :users, controllers: { sessions: 'sessions', registrations: 'registrations' }
   get 'errors/not_found'
   get 'errors/internal_server_error'
