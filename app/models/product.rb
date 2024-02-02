8b062d9d3d68b1ab5280049aec66a0884044dc30
Frank Vielma <frankvielma@gmail.com>
2024-02-01 21:37:42 -0400

    add product model and specs

---
+++ app/models/product.rb
@@ -0,0 +1,22 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id                  :bigint           not null, primary key
+#  title               :string
+#  description         :text
+#  state               :integer
+#  stock               :integer
+#  unit_price_cents    :integer
+#  unit_price_currency :integer          default("USD")
+#  category_id         :integer
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#
+class Product < ApplicationRecord
+  enum unit_price_currency: { USD: 0, EUR: 1, BTC: 2 }
+
+  validates :title, :description, :unit_price_cents, :unit_price_currency, presence: true
+
+  has_one_attached :image
+end
---
+++ spec/models/product_spec.rb
@@ -0,0 +1,36 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id                  :bigint           not null, primary key
+#  title               :string
+#  description         :text
+#  state               :integer
+#  stock               :integer
+#  unit_price_cents    :integer
+#  unit_price_currency :integer          default("USD")
+#  category_id         :integer
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  # Validations
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:title) }
+    it { is_expected.to validate_presence_of(:description) }
+    it { is_expected.to validate_presence_of(:unit_price_cents) }
+    it { is_expected.to validate_presence_of(:unit_price_currency) }
+  end
+
+  # Enum validation
+  describe 'unit_price_currency enum' do
+    it { is_expected.to define_enum_for(:unit_price_currency).with_values(USD: 0, EUR: 1, BTC: 2) }
+  end
+
+  # Image attachment
+  describe 'image attachment' do
+    it { is_expected.to have_one_attached(:image) }
+  end
+end
