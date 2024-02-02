4fb786402bb01361e8e0fd163d095c26f7f7a9b6
Frank Vielma <frankvielma@gmail.com>
2024-02-02 16:32:28 -0400

    add categories and constraints

---
+++ db/migrate/20240202173514_create_categories.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class CreateCategories < ActiveRecord::Migration[7.1]
+  def change
+    create_table :categories do |t|
+      t.string :name
+      t.text :description
+      t.integer :parent_category_id
+
+      t.timestamps
+    end
+  end
+end
