4d7ea546d6bc59f1bb6614f6e776f1921ab26bb0
Frank Vielma <frankvielma@gmail.com>
2024-02-01 21:37:42 -0400

    add product model and specs

---
+++ spec/factories/products.rb
@@ -0,0 +1,26 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id                  :bigint           not null, primary key
+#  title               :string
+#  description         :text
+#  state               :integer
+#  stock               :integer
+#  unit_price_cents    :integer
+#  unit_price_currency :integer          default("USD")
+#  category_id         :integer
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#
+FactoryBot.define do
+  factory :product do
+    title { "MyString" }
+    description { "MyText" }
+    state { 1 }
+    stock { 1 }
+    unit_price_cents { 1 }
+    unit_price_currency { "MyString" }
+    category_id { 1 }
+  end
+end
