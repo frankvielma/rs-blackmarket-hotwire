a191ae04f0cd7c9da64f6bc05ec737ef516b2fb3
Frank Vielma <frankvielma@gmail.com>
2024-02-07 12:58:47 -0400

    refactor favorite and add keypress event

--- app/components/see_all_products_component.rb
+++ app/components/see_all_products_component.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
 
 class SeeAllProductsComponent < ViewComponent::Base
-
 end
--- app/javascript/controllers/favorite_controller.js
+++ app/javascript/controllers/favorite_controller.js
@@ -3,20 +3,30 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="favorite"
 export default class extends Controller {
 
+  handleKeyPress(event) {
+    const productId = event.target.id.split('-')[1];
+    if ((event.key === ' ') || (event.key === 'Enter')) {
+      event.preventDefault();
+      this.sendFavoriteRequest(`/products/${productId}/favorite`, { favorite: this.isFavorite });
+    }
+  }
 
   togglefavorite(event) {
-    const productId = event.target.id.split('-')[1]
-
-    fetch(`/products/${productId}/favorite`, {
+    const productId = event.target.parentElement.id.split('-')[1];
+    this.sendFavoriteRequest(`/products/${productId}/favorite`, { favorite: this.isFavorite });
+  }
+  
+  sendFavoriteRequest(url, data) {
+    fetch(url, {
       method: "POST",
       headers: { "X-CSRF-Token": document.querySelector("[name='csrf-token']").content },
-      body: JSON.stringify({ favorite: this.isFavorite })
+      body: JSON.stringify(data)
     })
-      .then(response => response.json())
-      .then(data => {
-        this.element.children[0].classList.toggle('hidden');
-        this.element.children[1].classList.toggle('hidden');
-      })
-
+    .then(response => response.json())
+    .then(data => {
+      this.element.children[0].classList.toggle('hidden');
+      this.element.children[1].classList.toggle('hidden');
+    });
   }
+
 }
