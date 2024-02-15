bc139a4807e8c5273d8990f5c6ef6fc55fba2b32
Frank Vielma <frankvielma@gmail.com>
2024-02-15 10:23:55 -0400

    refactor and clear results after filter by condition

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,21 +4,31 @@ class ProductsController < ApplicationController
   include Devise::Controllers::Helpers
   before_action :authenticate_user!
 
+  # def index
+  #   query = params[:query]
+  #   state = params[:state]
+  #   products = query.present? || state.present? ? Product.search_products(query, state) : Product.all
+  #   @pagy, @products = pagy(products, items: 5) if products.present?
+
+  #   return if query.blank?
+
+  #   partial = if @products.present?
+  #               'products/search_results'
+  #             else
+  #               'products/empty'
+  #             end
+
+  #   render turbo_stream: turbo_stream.update('main', partial:, locals: { products:, query: })
+  # end
+
   def index
     query = params[:query]
     state = params[:state]
-    products = query.present? || state.present? ? Product.search_products(query, state) : Product.all
-    @pagy, @products = pagy(products, items: 5) if products.present?
-
+    products = search_products(query, state)
+    paginate_products(products)
     return if query.blank?
 
-    partial = if @products.present?
-                'products/search_results'
-              else
-                'products/empty'
-              end
-
-    render turbo_stream: turbo_stream.update('main', partial:, locals: { products:, query: })
+    render_search_results_partial(products, query)
   end
 
   def favorite
@@ -32,5 +42,20 @@ class ProductsController < ApplicationController
     end
 
     render json: {}
+  end
+
+  private
+
+  def search_products(query, state)
+    query.present? || state.present? ? Product.search_products(query, state) : Product.all
+  end
+
+  def paginate_products(products)
+    @pagy, @products = pagy(products, items: 5) if products.present?
+  end
+
+  def render_search_results_partial(products, query)
+    partial = products.present? ? 'products/search_results' : 'products/empty'
+    render turbo_stream: turbo_stream.update('main', partial:, locals: { products:, query: })
   end
 end
