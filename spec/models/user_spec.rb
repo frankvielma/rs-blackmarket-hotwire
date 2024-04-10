81164967d1af47c7e8e97cfce295c5626b3bf09f
Frank Vielma <frankvielma@gmail.com>
2024-04-10 15:07:11 -0400

    update user spec fix user full_name

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -23,18 +23,39 @@
 #
 
 RSpec.describe User do
-  it { is_expected.to validate_presence_of(:email) }
-  it { is_expected.to validate_presence_of(:encrypted_password) }
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_presence_of(:encrypted_password) }
+  end
+
+  describe 'associations' do
+    it { is_expected.to have_many(:favorite_products).dependent(:destroy) }
+    it { is_expected.to have_many(:shopping_carts).dependent(:destroy) }
+  end
 
   describe '#full_name' do
-    it 'returns username if first name is blank' do
-      user = described_class.new(username: 'testuser', first_name: '', last_name: 'Doe')
-      expect(user.full_name).to eq('testuser')
+    context 'with first and last name' do
+      let(:user) { described_class.create(first_name: 'John', last_name: 'Doe') }
+
+      it 'returns the full name' do
+        expect(user.full_name).to eq('John Doe')
+      end
     end
 
-    it 'returns full name if first name is present' do
-      user = described_class.new(username: 'testuser', first_name: 'John', last_name: 'Doe')
-      expect(user.full_name).to eq('John Doe')
+    context 'with only first name' do
+      let(:user) { described_class.create(first_name: 'John') }
+
+      it 'returns the first name' do
+        expect(user.full_name).to eq('John')
+      end
+    end
+
+    context 'with no name' do
+      let(:user) { described_class.create }
+
+      it 'returns an empty string' do
+        expect(user.full_name).to eq('')
+      end
     end
   end
 end
