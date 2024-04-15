c8465759ddb24c4e9a05516edebd1fff962f5cbe
Frank Vielma <frankvielma@gmail.com>
2024-04-15 15:18:14 -0400

    add total on shopping cart

--- app/controllers/shopping_cart_controller.rb
+++ app/controllers/shopping_cart_controller.rb
@@ -2,6 +2,7 @@
 
 class ShoppingCartController < ApplicationController
   def index
-    @cart_items = ShoppingCart.all
+    @cart_items = ShoppingCart.includes(:product).where(user_id: current_user)
+    @total = @cart_items.sum { |item| item.product.unit_price_cents } / 100.0
   end
 end
