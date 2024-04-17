6ef6863142e439b1a0be55e62a61cbfdb07c8806
Frank Vielma <frankvielma@gmail.com>
2024-04-17 09:17:04 -0400

    add cart component

--- app/controllers/shopping_cart_controller.rb
+++ app/controllers/shopping_cart_controller.rb
@@ -2,7 +2,13 @@
 
 class ShoppingCartController < ApplicationController
   def index
-    @cart_items = ShoppingCart.includes(:product).where(user_id: current_user)
-    @total = @cart_items.sum { |item| item.product.unit_price_cents } / 100.0
+    cart_items = ShoppingCart.includes(:product).where(user_id: current_user)
+    total = cart_items.sum { |item| item.product.unit_price_cents } / 100.0
+
+    partial = cart_items.present? ? 'shopping_cart/search_results' : 'shopping_cart/empty'
+    render turbo_stream: [
+      turbo_stream.replace('toggle-shopping-product', ProductButtonComponent.new),
+      turbo_stream.update('main', partial:, locals: { cart_items:, total: })
+    ]
   end
 end
