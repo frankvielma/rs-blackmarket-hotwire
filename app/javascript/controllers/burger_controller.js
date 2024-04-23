8d5536d4d0c05b65941dcb9635feb7aa508c0a82
Frank Vielma <frankvielma@gmail.com>
2024-04-23 10:50:58 -0400

    refactor burger controller

--- app/javascript/controllers/burger_controller.js
+++ app/javascript/controllers/burger_controller.js
@@ -1,33 +1,23 @@
 import { Controller } from "@hotwired/stimulus";
 
-// Connects to data-controller="burger"
 export default class extends Controller {
   connect() {
-    // open
     const burger = document.querySelectorAll(".navbar-burger");
     const menu = document.querySelectorAll(".navbar-menu");
 
     if (burger.length && menu.length) {
-      for (var i = 0; i < burger.length; i++) {
-        burger[i].addEventListener("click", function () {
-          for (var j = 0; j < menu.length; j++) {
-            menu[j].classList.toggle("hidden");
-          }
-        });
-      }
+      burger.forEach((item) => item.addEventListener("click", () => {
+        menu.forEach((menuItem) => menuItem.classList.toggle("hidden"));
+      }));
     }
 
-    // close
     const close = document.querySelectorAll(".navbar-close");
 
     if (close.length) {
-      for (var i = 0; i < close.length; i++) {
-        close[i].addEventListener("click", function () {
-          for (var j = 0; j < menu.length; j++) {
-            menu[j].classList.toggle("hidden");
-          }
-        });
-      }
+      close.forEach((item) => item.addEventListener("click", () => {
+        menu.forEach((menuItem) => menuItem.classList.toggle("hidden"));
+      }));
     }
   }
 }
+
