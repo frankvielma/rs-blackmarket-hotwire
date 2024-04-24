5443a3928842307de1d7a77beffd8b5ea36d2bb4
Frank Vielma <frankvielma@gmail.com>
2024-04-24 16:54:50 -0400

    remove dup code validate_form controller

--- app/javascript/controllers/validate_form_controller.js
+++ app/javascript/controllers/validate_form_controller.js
@@ -8,37 +8,55 @@ export default class extends Controller {
   }
 
   checkForm = () => {
-    let isValid = this.validateEmail() && this.validatePassword();
-    if (typeof user_password_confirmation !== "undefined") {
-      isValid = isValid && this.validateConfirmPassword()
+    let [user_email1, user_password1, user_password_confirmation1] = this.admin_form();
+    let isValid = this.validateEmail(user_email1) && this.validatePassword(user_password1);
+    if (typeof user_password_confirmation1 !== "undefined") {
+      isValid = isValid && this.validateConfirmPassword(user_password1, user_password_confirmation1)
     }
     if (isValid) {
-      error.innerText = '';
+      error.innerText = "";
       submitButton.disabled = false;
       alertText.classList.add("hidden");
       submitButton.className = "h-[44px] rounded-md text-white w-full bg-black font-bold text-white outline-none cursor-pointer active:ring-2 active:ring-offset-2 active:outline-none custom-focus hover:bg-hover active:bg-hover"
     }
+  };
+
+  admin_form() {
+    if (typeof new_admin_user !== "undefined") {
+      var user_email1 = admin_user_email;
+      var user_password1 = admin_user_password;
+      if (typeof admin_user_password_confirmation !== "undefined") {
+        var user_password_confirmation1 = admin_user_password_confirmation;
+      }
+    } else {
+      var user_email1 = user_email;
+      var user_password1 = user_password;
+      if (typeof user_password_confirmation !== "undefined") {
+        var user_password_confirmation1 = user_password_confirmation;
+      }
+    }
+    return [user_email1, user_password1, user_password_confirmation1];
   }
 
-  validateEmail() {
-    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
-    const isValid = emailRegex.test(user_email.value);
-    this.showValidationForTarget('email', isValid)
+  validateEmail(user_email1) {
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    const isValid = emailRegex.test(user_email1.value);
+    this.showValidationForTarget('email', isValid);
     return isValid
   }
 
-  validatePassword() {
-    const isValid = user_password.value.length >= 8
-    this.showValidationForTarget('password', isValid)
+  validatePassword(user_password) {
+    const isValid = user_password.value.length >= 8;
+    this.showValidationForTarget('password', isValid);
     return isValid
   }
 
-  validateConfirmPassword() {
+  validateConfirmPassword(user_password, user_password_confirmation) {
     const confirmPasswordInput = user_password_confirmation.value;
     const isPasswordLengthValid = confirmPasswordInput.length >= 8;
     const doPasswordsMatch = user_password.value === confirmPasswordInput;
 
-    this.showValidationForTarget('password', isPasswordLengthValid)
+    this.showValidationForTarget('password', isPasswordLengthValid);
 
     error.innerHTML = doPasswordsMatch ? "" : "Passwords do not match.";
     error.className = doPasswordsMatch
@@ -57,15 +75,16 @@ export default class extends Controller {
   }
 
   toggleVisibility(event) {
-    const isCurrentlyPassword = user_password.type === "password";
+    let [user_email1, user_password1, user_password_confirmation1] = this.admin_form();
+    const isCurrentlyPassword = user_password1.type === "password";
     const newType = isCurrentlyPassword ? "text" : "password";
 
     this.findVisibilityOff().forEach(img => img.classList.toggle('hidden'));
     this.findVisibilityOn().forEach(img => img.classList.toggle('hidden'));
 
-    user_password.type = newType;
-    if (typeof user_password_confirmation !== "undefined") {
-      user_password_confirmation.type = newType;
+    user_password1.type = newType;
+    if (typeof user_password_confirmation1 !== "undefined") {
+      user_password_confirmation1.type = newType;
     }
   }
 
