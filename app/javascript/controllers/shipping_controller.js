dde782bed9e009690fbc91be807e6874744cfb08
Frank Vielma <frankvielma@gmail.com>
2024-04-29 09:30:30 -0400

    remove it

--- app/javascript/controllers/shipping_controller.js
+++ app/javascript/controllers/shipping_controller.js
@@ -2,8 +2,6 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="shipping"
 export default class extends Controller {
-  static tar
-
   cancel() {
     Turbo.visit('/shopping_cart/index');
   }
@@ -11,5 +9,4 @@ export default class extends Controller {
   billing() {
     billingAddress.classList.toggle('hidden');
   }
-
 }
