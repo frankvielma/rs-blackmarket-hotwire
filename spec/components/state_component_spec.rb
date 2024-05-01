7c03ccc2aea72122e6b1d804cff04ee3ed802c95
Frank Vielma <frankvielma@gmail.com>
2024-05-01 12:08:06 -0400

    add state_component spec

--- spec/components/state_component_spec.rb
+++ spec/components/state_component_spec.rb
@@ -3,13 +3,31 @@
 require 'rails_helper'
 
 RSpec.describe StateComponent, type: :component do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'with different state values' do
+    let(:states) { %w[restored is_new used] }
 
-  # it "renders something useful" do
-  #   expect(
-  #     render_inline(described_class.new(attr: "value")) { "Hello, components!" }.css("p").to_html
-  #   ).to include(
-  #     "Hello, components!"
-  #   )
-  # end
+    it 'renders the correct background color for each state' do
+      states.each do |state|
+        rendered = render_inline(described_class.new(state:))
+        expected_color = 'bg-[' + color(state) + ']'
+        expect(rendered).to have_css('div', class: expected_color)
+      end
+    end
+
+    it "renders the state text except for 'is_new'" do
+      states.each do |state|
+        expected_text = state == 'is_new' ? 'new' : state
+        rendered = render_inline(described_class.new(state:))
+        expect(rendered).to have_text(expected_text)
+      end
+    end
+  end
+
+  def color(state)
+    {
+      'restored' => '#559F21',
+      'is_new' => '#2751B9',
+      'used' => '#D42F1A'
+    }[state]
+  end
 end
