8f332b27f7fa147e9ee749d0b30ae3f5c6adde0c
Frank Vielma <frankvielma@gmail.com>
2024-05-01 12:13:52 -0400

    add product button component spec

--- spec/components/product_button_component_spec.rb
+++ spec/components/product_button_component_spec.rb
@@ -3,13 +3,9 @@
 require 'rails_helper'
 
 RSpec.describe ProductButtonComponent, type: :component do
-  pending "add some examples to (or delete) #{__FILE__}"
-
-  # it "renders something useful" do
-  #   expect(
-  #     render_inline(described_class.new(attr: "value")) { "Hello, components!" }.css("p").to_html
-  #   ).to include(
-  #     "Hello, components!"
-  #   )
-  # end
+  it 'renders the button' do
+    render_inline(described_class.new)
+    expect(page).to have_text('Products')
+    expect(page).to have_button
+  end
 end
