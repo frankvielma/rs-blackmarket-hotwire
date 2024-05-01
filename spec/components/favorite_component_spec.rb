628996b3e05dd6322cfb847707cdde10ff77875a
Frank Vielma <frankvielma@gmail.com>
2024-05-01 11:55:24 -0400

    add favorite_component spec

--- spec/components/favorite_component_spec.rb
+++ spec/components/favorite_component_spec.rb
@@ -3,13 +3,36 @@
 require 'rails_helper'
 
 RSpec.describe FavoriteComponent, type: :component do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user, password: 'password', password_confirmation: 'password') }
+  let(:product) { create(:product) }
 
-  # it "renders something useful" do
-  #   expect(
-  #     render_inline(described_class.new(attr: "value")) { "Hello, components!" }.css("p").to_html
-  #   ).to include(
-  #     "Hello, components!"
-  #   )
-  # end
+  before do
+    sign_in user
+  end
+
+  describe 'when product is not favorited' do
+    before do
+      allow(FavoriteProduct).to receive(:where).and_return([])
+    end
+
+    it 'renders favorite_off class' do
+      rendered = render_inline(described_class.new(product_id: product.id))
+
+      expect(rendered).to have_xpath("//img[1][contains(@class, '')]")
+      expect(rendered).to have_xpath("//img[2][contains(@class, 'hidden')]")
+    end
+  end
+
+  describe 'when product is favorited' do
+    before do
+      allow(FavoriteProduct).to receive(:where).and_return([instance_double('FavoriteProduct')]) # Favorite exists
+    end
+
+    it 'renders favorite_on class' do
+      rendered = render_inline(described_class.new(product_id: product.id))
+
+      expect(rendered).to have_xpath("//img[1][contains(@class, 'hidden')]")
+      expect(rendered).to have_xpath("//img[2][contains(@class, '')]")
+    end
+  end
 end
