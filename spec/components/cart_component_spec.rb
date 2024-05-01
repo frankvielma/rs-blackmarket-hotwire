1664fcaddfc2226575da57da371f06f2668660c4
Frank Vielma <frankvielma@gmail.com>
2024-05-01 11:29:47 -0400

    add cart_component spec

--- spec/components/cart_component_spec.rb
+++ spec/components/cart_component_spec.rb
@@ -3,13 +3,20 @@
 require 'rails_helper'
 
 RSpec.describe CartComponent, type: :component do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user, password: 'password', password_confirmation: 'password') }
+  let(:product) { create(:product) }
+  let!(:cart_item) { create(:shopping_cart, user:, product:) }
+  let(:line_item) { create(:line_item, product:, shopping_cart: cart_item) }
 
-  # it "renders something useful" do
-  #   expect(
-  #     render_inline(described_class.new(attr: "value")) { "Hello, components!" }.css("p").to_html
-  #   ).to include(
-  #     "Hello, components!"
-  #   )
-  # end
+  before do
+    product.image.attach(fixture_file_upload('image.jpg', 'image/jpeg'))
+    line_item.quantity = 2
+    sign_in user
+  end
+
+  it 'renders the product name' do
+    rendered = render_inline(described_class.new(product:))
+
+    expect(rendered).to have_text product.title.truncate(10)
+  end
 end
